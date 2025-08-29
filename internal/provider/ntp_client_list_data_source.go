c3337f3da57fca43a2e324b4ce718c3c945620d9
hellt <dodin.roman@gmail.com>
2025-08-29 19:00:17 +0200

    Sync 20250829-190017

---
+++ internal/provider/ntp_client_list_data_source.go
@@ -0,0 +1,109 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/nokia/eda/apps/terraform-provider-timing/internal/datasource_ntp_client_list"
+	"github.com/nokia/eda/apps/terraform-provider-timing/internal/eda/apiclient"
+	"github.com/nokia/eda/apps/terraform-provider-timing/internal/tfutils"
+)
+
+const read_ds_ntpClientList = "/apps/timing.eda.nokia.com/v1alpha1/namespaces/{namespace}/ntpclients"
+
+var (
+	_ datasource.DataSource              = (*ntpClientListDataSource)(nil)
+	_ datasource.DataSourceWithConfigure = (*ntpClientListDataSource)(nil)
+)
+
+func NewNtpClientListDataSource() datasource.DataSource {
+	return &ntpClientListDataSource{}
+}
+
+type ntpClientListDataSource struct {
+	client *apiclient.EdaApiClient
+}
+
+func (d *ntpClientListDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_ntp_client_list"
+}
+
+func (d *ntpClientListDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = datasource_ntp_client_list.NtpClientListDataSourceSchema(ctx)
+}
+
+func (d *ntpClientListDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data datasource_ntp_client_list.NtpClientListModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Extract query params from Terraform model
+	queryParams, err := tfutils.ModelToStringMap(ctx, &data)
+	if err != nil {
+		resp.Diagnostics.AddError("Error extracting query params", err.Error())
+		return
+	}
+
+	// Read API call logic
+	tflog.Info(ctx, "Read()::API request", map[string]any{
+		"path":  read_ds_ntpClientList,
+		"data":  spew.Sdump(data),
+		"query": queryParams,
+	})
+
+	t0 := time.Now()
+	result := map[string]any{}
+	err = d.client.GetByQuery(ctx, read_ds_ntpClientList, map[string]string{
+		"namespace": tfutils.StringValue(data.Namespace),
+	}, queryParams, &result)
+
+	tflog.Info(ctx, "Read()::API returned", map[string]any{
+		"path":      read_ds_ntpClientList,
+		"result":    spew.Sdump(result),
+		"timeTaken": time.Since(t0).String(),
+	})
+
+	if err != nil {
+		resp.Diagnostics.AddError("Error reading resource", err.Error())
+		return
+	}
+
+	// Convert API response to Terraform model
+	err = tfutils.AnyMapToModel(ctx, result, &data)
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to build response from API result", err.Error())
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *ntpClientListDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Add a nil check when handling ProviderData because Terraform
+	// sets that data after it calls the ConfigureProvider RPC.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*apiclient.EdaApiClient)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *api.EdaApiClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+	r.client = client
+}
